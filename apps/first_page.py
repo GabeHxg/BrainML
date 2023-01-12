d894c70022611af124a6a106739c61cb592658cc
gabrielhxg <hgomesgabriel@gmail.com>
2023-01-12 12:52:13 -0300

    thx

--- apps/first_page.py
+++ apps/first_page.py
@@ -1,10 +1,22 @@
+# Importing the required libraries
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
 import streamlit as st
 
 def app():
-    st.title("Candidatos e suas campanhas")
+    st.title("Access Data")
 
     st.markdown(
         """
-    **Estudo considerando 2963 Candidatos**
+    **Estudo considerando x casas**
     """
     )
+
+    # Reading the dataset
+    data = pd.read_csv('data/BHdata.csv')
+
+    # Print it out
+    st.dataframe(data)
