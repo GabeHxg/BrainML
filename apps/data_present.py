05a32470a39e21078b6ad69922d222e487683ba1
gabrielhxg <hgomesgabriel@gmail.com>
2023-02-14 20:10:12 -0300

    Transfering

---
+++ apps/data_present.py
@@ -0,0 +1,32 @@
+# Importing the required libraries
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+def app():
+    st.title("Access Data")
+
+    st.markdown(
+        """
+    **Teste | Sistema Web Jurisprudência**
+    """
+    )
+
+    # Formatação para aparência da análise
+    # cores
+
+    # Reading the dataset
+
+
+    # Print it out
+    st.markdown('### Original')
+    #st.dataframe(df)
+    st.markdown('### Colunas')
+    #st.markdown(list(df.columns))
+
+    # EDA
+
+
+
+
+
--- base_app.py
+++ base_app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-from apps import first_page  # import your app modules here
+from apps import data_present  # import your app modules here
 
 st.set_page_config(page_title="Streamlit Geospatial", layout="wide")
 
 apps = [
-    {"func": first_page.app, "title": "first_page", "icon": "people"},
-]
+    {"func": data_present.app, "title": "Apresentação dos dados", "icon": "file-earmark-bar-graph"},
+]  # Icons: https://icons.getbootstrap.com/
+
 
 titles = [app["title"] for app in apps]
 titles_lower = [title.lower() for title in titles]
@@ -21,7 +22,7 @@ else:
 
 with st.sidebar:
     selected = option_menu(
-        "Brain MLOps",
+        "Jurimetria",
         options=titles,
         icons=icons,
         menu_icon="menu-button-fill",
@@ -31,11 +32,9 @@ with st.sidebar:
     st.sidebar.title("Sobre")
     st.sidebar.info(
         """
-        Demo foi criada por [Gabriel Hxg](https://gabrielhxg.carrd.co/) 
-        como forma de apresentação do potencial de ml em Real Estate Analytics. 
-
-        Dados Fonte: [text](https://google.com)
-    """
+        Sistema Web Jurisprudência e Confecção de Manifestações. 
+        Análise de decisões de Tribunais de Contas. 
+        """
     )
 
 for app in apps:
